8cdf8fd0ad7448680b468b52626cfe134cfdbddb
jfgagnon <jfgagnon_00@yahoo.com>
2024-06-02 13:07:49 -0400

    Ajout test environment

--- .ci/test_python_interpreter.sh
+++ .ci/test_python_interpreter.sh
@@ -3,7 +3,7 @@
 # script verifie si python est accessible
 PYTHON_INTERPRETER=$1
 
-ver=$($PYTHON_INTERPRETER -c "import sys; print(sys.version_info.major)")
+ver=`$PYTHON_INTERPRETER -c "import sys; print(sys.version_info.major)" > /dev/null 2>&1`
 if [ -z "$ver" ]; then
 	exit 1
 fi
--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,11 @@
 source configs/variables.sh
 
+${CI_DIR}/test_python_interpreter.sh
+if [ $? -ne 0 ]; then
+    echo "$PYTHON_INTERPRETER n'est pas accessible"
+	exit 1
+fi
+
 $PYTHON_INTERPRETER -m pip install -q virtualenv
 
 echo "source ${CI_DIR}/activate_environment.sh ${PYTHON_INTERPRETER} ${PROJECT_NAME}" > activate.sh
