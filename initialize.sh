57bc96e0f1c8f7b7fa2cfde08a0fb575a79652ba
jfgagnon <jfgagnon_00@yahoo.com>
2024-06-02 11:55:58 -0400

    Fix pour script initialization

--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,7 @@
-source .configs/variables.sh
+source configs/variables.sh
 
-$(PYTHON_INTERPRETER) -m pip install -q virtualenv
+$PYTHON_INTERPRETER -m pip install -q virtualenv
 
-@echo "source $(CI_DIR)/activate_environment.sh $(PYTHON_INTERPRETER) $(PROJECT_NAME)" > activate.sh
-@chmod 755 activate.sh
-@echo \"source activate.sh\" pour activer environment python
+echo "source ${CI_DIR}/activate_environment.sh ${PYTHON_INTERPRETER} ${PROJECT_NAME}" > activate.sh
+chmod 755 activate.sh
+echo \"source activate.sh\" pour activer environment python
