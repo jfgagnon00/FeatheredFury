f94d17ee89ec427019b2669268691fb2071d322a
jfgagnon <jfgagnon_00@yahoo.com>
2024-07-03 11:57:00 -0400

    Mise a jour requirments; probleme avec path pour kernel jupyter

--- .ci/activate_environment.sh
+++ .ci/activate_environment.sh
@@ -19,7 +19,7 @@ if [ $InVenv -eq 1 ]; then
     return
 fi
 
-# verifier si snake environment existe
+# verifier si environment existe
 NeedInstall=0
 if ! [ -f .venv/bin/activate ]; then
     NeedInstall=1
@@ -41,4 +41,7 @@ if [ $NeedInstall -eq 1 ]; then
     echo "Installation des dépendences"
     $PYTHON_INTERPRETER -m pip install --upgrade pip
     $PYTHON_INTERPRETER -m pip install -r requirements.txt
+
+    # s'assurer que les jupyter notebook pointent aussi sur bon environment
+    $PYTHON_INTERPRETER -m ipykernel install --sys-prefix --name $PYTHON_INTERPRETER --display-name $ENV_NAME
 fi
