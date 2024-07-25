b8d5d4f83700993a944c8b4cb62c348b2a279d52
jfgagnon <jfgagnon_00@yahoo.com>
2024-07-25 16:23:33 -0400

    Explorer solution pour preprocess one hot encode + spectrogram

--- src/ffury/dataset/_preprocess.py
+++ src/ffury/dataset/_preprocess.py
@@ -1,11 +1,19 @@
 from logging import Logger
 from pandas import DataFrame
+from tqdm import tqdm
 
 from ..configs import PreprocessConfig
+from ..misc.concurrent import (
+    create_thread_pool_executor,
+    parallel_for
+)
+
 
 def _preprocess_parallel_dataframe(logger: Logger,
-                                config: PreprocessConfig, 
-                                data: DataFrame) -> None:
+                                   config: PreprocessConfig, 
+                                   data: DataFrame) -> None:
+    # une autre table qui a nom commun et one hot encode
+    # mon data est spectrogram, lat, long, espece (one hot encode)
     pass
 
 def preprocess_parallel(logger: Logger,
@@ -13,8 +21,9 @@ def preprocess_parallel(logger: Logger,
                         train: DataFrame,
                         test: DataFrame,
                         validation: DataFrame) -> None:
-    for data, name in [ (train, "train"), 
-                        (test, "test"),
-                        (validation, "validation") ]:
-        logger.info(f"Processing {name}")
-        _preprocess_parallel_dataframe(logger, config, data)
+    with create_thread_pool_executor() as executor:
+        for data, name in [ (train, "train"), 
+                            (test, "test"),
+                            (validation, "validation") ]:
+            logger.info(f"Processing {name}")
+            _preprocess_parallel_dataframe(logger, config, data)
