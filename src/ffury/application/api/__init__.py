b67dcedfffd7bbaec00da03421c8613a9434d7e0
jfgagnon <jfgagnon_00@yahoo.com>
2024-12-08 19:17:07 -0500

    Mise a jour requirements pour service dans ffury

--- src/ffury/application/api/__init__.py
+++ src/ffury/application/api/__init__.py
@@ -3,6 +3,5 @@ from .routes import api_routes
 
 def create_api():
     api = Flask(__name__)
-    #api.config.from_object('config.api_config')
     api.register_blueprint(api_routes)
     return api
