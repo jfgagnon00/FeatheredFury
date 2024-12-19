f11a0e67bb859f29f10e1c7c8c8a90327668073e
jfgagnon <jfgagnon_00@yahoo.com>
2024-12-18 23:00:29 -0500

    Refactor app pour simplifier

--- src/ffury/application/__init__.py
+++ src/ffury/application/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 from pathlib import Path
 
 from .blueprints.home.routes import home
-from .blueprints.auth.routes import auth
 from .blueprints.model.routes import model
 # from app.blueprints.predict.routes import predict
 
 from ..configs import ProjectConfig
+from ..misc.logging import create_logger
 
 def create_flask_app(project_config: ProjectConfig,
                      secret: str) -> Flask:
@@ -18,11 +18,16 @@ def create_flask_app(project_config: ProjectConfig,
     app.config["UPLOAD_FOLDER"] = upload
     app.config["MAX_CONTENT_LENGTH"] = 4 * 1024 * 1024  # 4 Mo
     app.config["ALLOWED_EXTENSIONS"] = {"wav", "mp3", "ogg"}
+    app.config["LOGGER"] = create_logger(file=__file__)
 
     # Enregistrer les Blueprints
     app.register_blueprint(home, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/auth")
     app.register_blueprint(model, url_prefix="/model")
     # app.register_blueprint(predict, url_prefix="/predict")
+
+    @app.context_processor
+    def inject_context():
+        return dict(name=project_config.paths.PROJECT_NAME, 
+                    year=2024)
 
     return app
--- src/ffury/application/blueprints/home/routes.py
+++ src/ffury/application/blueprints/home/routes.py
@@ -1,12 +1,11 @@
-from flask import Blueprint, render_template, session
-import logging
+from flask import (
+    Blueprint, 
+    render_template
+)
 
-home = Blueprint('home', __name__)#, template_folder='../templates/home')
-
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+home = Blueprint('home', __name__)
 
 @home.route('/', methods=['GET'])
 def index(): 
-    logging.info(session.get('user'))
     return render_template('home/index.html')
  
