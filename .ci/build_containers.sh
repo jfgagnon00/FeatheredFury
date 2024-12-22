908ca263638be04e96ad1a656f248248c09cbdb5
jfgagnon <jfgagnon_00@yahoo.com>
2024-12-22 16:19:27 -0500

    Helper script

---
+++ .ci/build_containers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# hardcode pour le moment - suppose etre appeler a la racine du projet
+docker build -t application \
+             -f .ci/containers/docker-application \
+             --build-arg APPLICATION_PORT=5000 \
+             --build-arg SERVICE_PORT=5001 \
+             .
+
+docker build -t service \
+             -f .ci/containers/docker-service \
+             --build-arg SERVICE_PORT=5001 \
+             .
