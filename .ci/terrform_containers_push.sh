bf2f4b39d96b7bc07925e860be26d5bc635b0cc4
jfgagnon <jfgagnon_00@yahoo.com>
2025-01-12 16:35:17 -0500

    Test workflow staging

--- .ci/terrform_containers_push.sh
+++ .ci/terrform_containers_push.sh
@@ -2,15 +2,18 @@
 
 DIR=$(dirname "$0")
 
-# echo ${FFURY_REGISTRY_SERVER}
-# echo ${FFURY_REGISTRY_NAME}
+echo ${FFURY_REGISTRY_SERVER}
+echo ${FFURY_REGISTRY_NAME}
 
 ${DIR}/containers_command.sh --azure build
 
+echo "Login Azure ACR"
 az acr login --name ${FFURY_REGISTRY_NAME}
 
+echo "Docker tag"
 docker tag ffury-web_app:latest ${FFURY_REGISTRY_SERVER}/ffury-web_app:latest
 docker tag ffury-web_service:latest ${FFURY_REGISTRY_SERVER}/ffury-web_service:latest
 
+echo "Docker push"
 docker push ${FFURY_REGISTRY_SERVER}/ffury-web_app:latest
 docker push ${FFURY_REGISTRY_SERVER}/ffury-web_service:latest
