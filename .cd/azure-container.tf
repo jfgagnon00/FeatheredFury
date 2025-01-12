2fcb3e5236171d45ff9a5bc3aa34e6644cc04bcc
jfgagnon <jfgagnon_00@yahoo.com>
2025-01-12 18:50:27 -0500

    Test workflow staging

--- .cd/azure-container.tf
+++ .cd/azure-container.tf
@@ -76,3 +76,8 @@ resource "azurerm_container_group" "acg" {
     null_resource.docker_push
   ]
 }
+
+# import {
+#   to = azurerm_container_group.acg
+#   id = "${data.azurerm_subscription.current.id}/resourceGroups/${local.resource_group_name}/providers/Microsoft.ContainerInstance/containerGroups/${local.container_instance_name}"
+# }
