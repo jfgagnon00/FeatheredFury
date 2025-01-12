367b722eaad66bda14675e3c02bc4205d8d3e1b1
jfgagnon <jfgagnon_00@yahoo.com>
2025-01-12 17:25:28 -0500

    Test workflow staging

--- .ci/containers_command.sh
+++ .ci/containers_command.sh
@@ -17,7 +17,11 @@ fi
 # echo "'$ENVIRONMENT'"
 # echo "'$@'"
 
+env | grep FFURY
 
+ls -la
+
+cd ../ci/containers
 echo "docker-compose --env-file ${DIR}/containers/.env --env-file ${DIR}/containers/${EnvironmentEnvFileOverrides} -f ${DIR}/containers/docker-compose.yaml $@"
 
 docker-compose \
