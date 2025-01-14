23baa756ccbdf6a3c50e83b921ec2b46ce9b41e9
jfgagnon <jfgagnon_00@yahoo.com>
2025-01-13 21:29:54 -0500

    Cleanup script

--- .cd/terraform_destroy_previous.sh
+++ .cd/terraform_destroy_previous.sh
@@ -2,19 +2,18 @@
 
 terraform apply -auto-approve -refresh-only
 
+# obtenir info pour destruction anciennes resources azure
 SUBSCRIPTION=$(terraform output -json subscription | tr -d '"')
 ENVIRONMENT=$(terraform output -json environment | tr -d '"')
 
-echo ${SUBSCRIPTION}
-echo ${ENVIRONMENT}
-
+# mettre a jour le plan terraform
 terraform import azurerm_resource_group.rg  "${SUBSCRIPTION}/resourceGroups/ffury-${ENVIRONMENT}-rg"
-if [ $? -ne 0 ]; then
-    exit 0
-fi
-
 terraform import azurerm_container_registry.acr "${SUBSCRIPTION}/resourceGroups/ffury-${ENVIRONMENT}-rg/providers/Microsoft.ContainerRegistry/registries/ffury${ENVIRONMENT}acr"
 terraform import azurerm_container_group.acg  "${SUBSCRIPTION}/resourceGroups/ffury-${ENVIRONMENT}-rg/providers/Microsoft.ContainerInstance/containerGroups/ffury-${ENVIRONMENT}-instance"
+
+# destruction - pas important si ca existe ou non, ca va etre detruit si ca existe
 terraform destroy -auto-approve
 
+# pas vraiment d'erreur
+# s'il y avait rien, on a rien detruit sinon, on a fait ce qui etait demande
 exit 0
