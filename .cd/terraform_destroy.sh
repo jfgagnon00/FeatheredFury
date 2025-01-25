25675c617bfce90a93664582ed8ae2dc92a4a175
jfgagnon <jfgagnon_00@yahoo.com>
2025-01-25 15:50:32 -0500

    Enlever script redondant

--- .cd/terraform_destroy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# destruction - pas important si ca existe ou non, ca va etre detruit si ca existe
-terraform destroy -auto-approve
-
-# pas vraiment d'erreur
-# s'il y avait rien, on a rien detruit sinon, on a fait ce qui etait demande
-exit 0
